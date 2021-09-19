78927a29e98eb5b19f02e4061917411b2c972971
Pimpraethong Rotpotchanarat <86197283+RealMaysa@users.noreply.github.com>
2021-09-19 22:50:28 +0700

    Create perw_02.c

---
+++ perw_02.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+int arr[10];
+double sum;
+double avgCM;
+for(int i=0;i<10;++i){
+     printf("Enter your height: ");
+     scanf("%d",&arr[i]);
+     sum+=arr[i];
+    
+printf("Height: %d\n",arr[i]);
+
+}
+avgCM=sum/10.0;
+printf("Average height: %.2f", avgCM);
+    FILE*f=fopen("data.txt","w");
+    fprintf(f,"Average height: %.2f",avgCM);
+    return 0;
+
+}
